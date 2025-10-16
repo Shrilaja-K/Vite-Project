a1414daf116e2c8cd42088759380c31ca6b2f76b
Shrilaja-K <shrilaja.k@rently.com>
2025-10-16 14:03:32 +0530

    Added

--- React3/src/App.jsx
+++ React3/src/App.jsx
@@ -1,34 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect } from 'react'
+import { useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
 
+const App = () => {
+  
+   const [num1, setnum1] = useState(100);
+   const [num2, setnum2] = useState(1000);
+
+   useEffect(()=>{
+      setnum1(200);
+      console.log("from useEffect");
+      return ()=>{
+        console.log("memory cleaned");
+      }
+
+   },[num1,num2]); 
+  
+  //  console.log("num1",num1);
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>{num1}</h1>
+    <button onClick={() => setnum1((cur)=>cur+1)}>Add</button>
+    <h1>{num2}</h1>
+    <button onClick={() => setnum2((cur)=>cur+1)}>Add</button>
+    </div>
   )
 }
 

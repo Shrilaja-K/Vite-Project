2964aeea0e15a5cd861fa6886fe9356836a55e0f
Shrilaja-K <shrilaja.k@rently.com>
2025-10-16 17:54:27 +0530

    Added

---
+++ React4/src/App.jsx
@@ -0,0 +1,14 @@
+import FunctionalComponent from './FunctionalComponent';
+import ClassComponent from './ClassComponent';
+
+function App() {
+  return (
+    <div>
+      <h1>This is App</h1>
+      <FunctionalComponent />
+      <ClassComponent />
+    </div>
+  );
+}
+
+export default App;
